f0c18b03bdfd96e391cbd9af40d6f9c336afd2fc
Sean P. Kane <spkane00@gmail.com>
2024-01-26 14:22:09 -0800

    mongodb 5.0 arm64

--- scripts/shell_deploy.sh
+++ scripts/shell_deploy.sh
@@ -15,7 +15,7 @@ if [ $# -ne 0 ] && [ ${1} == "down" ]; then
   docker container rm -f hubot || true
   docker container rm -f zmachine || true
   docker container rm -f rocketchat || true
-  docker container rm -f mongo || true
+  docker container rm -f mongodb || true
   docker network rm botnet || true
   echo "Environment torn down..."
   exit 0
@@ -24,8 +24,8 @@ fi
 # Global Settings
 export PORT="3000"
 export ROOT_URL="http://127.0.0.1:3000"
-export MONGO_URL="mongodb://mongo:27017/rocketchat?replicaSet=rs0"
-export MONGO_OPLOG_URL="mongodb://mongo:27017/local?replicaSet=rs0"
+export MONGO_URL="mongodb://mongodb:27017/rocketchat?replicaSet=rs0"
+export MONGO_OPLOG_URL="mongodb://mongodb:27017/local?replicaSet=rs0"
 export MAIL_URL="smtp://smtp.email"
 export RESPOND_TO_DM="true"
 export HUBOT_ALIAS=". "
@@ -41,28 +41,28 @@ export HUBOT_ZMACHINE_SERVER="http://zmachine:80"
 export HUBOT_ZMACHINE_ROOMS="zmachine"
 export HUBOT_ZMACHINE_OT_PREFIX="ot"
 
-docker image build -t spkane/mongo:4.4 ./mongodb/docker
+docker image build -t spkane/mongo:5.0 ./mongodb/docker
 
-docker image push spkane/mongo:4.4
+docker image push spkane/mongo:5.0
 docker image pull spkane/zmachine-api:latest
-docker image pull rocketchat/rocket.chat:5.0.4
+docker image pull rocketchat/rocket.chat:6.5.2
 docker image pull rocketchat/hubot-rocketchat:latest
 
 docker container rm -f hubot || true
 docker container rm -f zmachine || true
 docker container rm -f rocketchat || true
-docker container rm -f mongo || true
+docker container rm -f mongodb || true
 
 docker network rm botnet || true
 
 docker network create -d bridge botnet
 
 docker container run -d \
-  --name=mongo \
+  --name=mongodb \
   --network=botnet \
   --restart unless-stopped \
   -v $(pwd)/mongodb/data/db:/bitnami/mongodb \
-  spkane/mongo:4.4 \
+  spkane/mongo:5.0 \
   mongod --oplogSize 128 --replSet rs0
 sleep 5
 docker container run -d \
@@ -76,7 +76,7 @@ docker container run -d \
   -e MONGO_URL=${MONGO_URL} \
   -e MONGO_OPLOG_URL=${MONGO_OPLOG_URL} \
   -e MAIL_URL=${MAIL_URL} \
-  rocketchat/rocket.chat:5.0.4
+  rocketchat/rocket.chat:6.5.2
 docker container run -d \
   --name=zmachine \
   --network=botnet \
